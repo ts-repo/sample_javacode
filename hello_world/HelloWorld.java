53b44c3829698755f9089ceb15c218cdb378d55c
Yuru <47292936+ts-repo@users.noreply.github.com>
2023-04-10 01:00:51 +0900

    HelloWorld.java を更新

--- hello_world/HelloWorld.java
+++ hello_world/HelloWorld.java
@@ -14,7 +14,135 @@ public class HelloWorld {
         }
 
         System.out.println("end");
+
+
+
+java listからランダムに4件取得する方法
+
+Javaのリスト（List）からランダムに4件の要素を取得する方法はいくつかあります。以下にいくつかの方法を示します。
+
+Collections.shuffle() メソッドを使用する方法:
+java
+Copy code
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>();
+        // リストに要素を追加する
+        // ...
+        
+        // リストをシャッフルする
+        Collections.shuffle(list);
+        
+        // ランダムに4件の要素を取得する
+        List<String> randomItems = list.subList(0, 4);
+        // randomItemsにはリストからランダムに4件の要素が含まれている
     }
+}
+java.util.Random クラスを使用する方法:
+java
+Copy code
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class Main {
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>();
+        // リストに要素を追加する
+        // ...
+        
+        // ランダムに4件のインデックスを生成する
+        List<Integer> indexes = new ArrayList<>();
+        Random random = new Random();
+        while (indexes.size() < 4) {
+            int index = random.nextInt(list.size());
+            if (!indexes.contains(index)) {
+                indexes.add(index);
+            }
+        }
+        
+        // ランダムに4件の要素を取得する
+        List<String> randomItems = new ArrayList<>();
+        for (int index : indexes) {
+            randomItems.add(list.get(index));
+        }
+        // randomItemsにはリストからランダムに4件の要素が含まれている
+    }
+}
+Stream API を使用する方法:
+java
+Copy code
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.stream.Collectors;
+
+public class Main {
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>();
+        // リストに要素を追加する
+        // ...
+        
+        // ランダムに4件の要素を取得する
+        Random random = new Random();
+        List<String> randomItems = list.stream()
+                .distinct()
+                .limit(4)
+                .collect(Collectors.toList());
+        // randomItemsにはリストからランダムに4件の要素が含まれている
+    }
+}
+これらの方法のいずれかを使用して、Javaのリストからランダムに4件の要素を取得することができます。
+
+
+
+tt.soccer.022484@gmail.com
+リストからランダムに最大4件取得する方法を教えてください。
+0件の場合はnullで処理が落ちないようにしてください。
+1件、2件、3件しか取得できない場合はその件数のみ取得してください
+
+Javaのリスト（List）から最大で4件の要素をランダムに取得する方法を示します。また、リストが空の場合にはnullを返さず、0件、1件、2件、3件の場合にはその件数のみを取得するようにします。
+
+java
+Copy code
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+
+public class Main {
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>();
+        // リストに要素を追加する
+        // ...
+
+        // リストのサイズを取得
+        int listSize = list.size();
+
+        // ランダムに取得する要素の件数を決定
+        int numItems = Math.min(listSize, 4);
+
+        // ランダムにインデックスをシャッフル
+        Collections.shuffle(list);
+        List<String> randomItems = list.subList(0, numItems);
+
+        // randomItemsにはリストからランダムに最大4件の要素が含まれている
+        if (randomItems.isEmpty()) {
+            // 0件の場合はnullを返す
+            randomItems = null;
+        }
+    }
+}
+このコードでは、リストのサイズを取得し、ランダムに取得する要素の件数を Math.min() メソッドを使用してリストのサイズと4のうち小さい方で決定します。その後、リストの要素をシャッフルし、最大で4件の要素を取得しています。また、ランダムに取得する要素の件数が0件の場合にはnullを返しています
+
+
+    }
+
+
 } 
 
 
